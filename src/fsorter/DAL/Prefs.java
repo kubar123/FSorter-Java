332e7dbcccbc67ca4bfa6bd27bd1dfc1b9d40701
Kubar123 <kubar123@hotmail.com>
2019-11-30 23:26:22 +1100

    Added preferences file for saving/loading prefs

---
+++ src/fsorter/DAL/Prefs.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fsorter.DAL;
+
+import java.util.prefs.Preferences;
+
+/**
+ *
+ * @author Jakub Rybicki <Jake at lansoftprogramming.com>
+ */
+public class Prefs {
+    public static void savePreference(String key,String value) {
+        Preferences prefs = Preferences.userNodeForPackage(Prefs.class);
+
+        prefs.put(key,value);
+    }
+
+    public static String readPreference(String key) {
+        Preferences prefs = Preferences.userNodeForPackage(Prefs.class);
+
+        return prefs.get(key, null);
+    }
+    
+    public static String getUpdatesAtStartup(){
+        Preferences prefs = Preferences.userNodeForPackage(Prefs.class);
+        return prefs.get("updateAtStartup","true");
+    }
+    
+}
